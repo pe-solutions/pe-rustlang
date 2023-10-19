6298e8ea1f6d692638003187b4dcc7aacf37ec69
menjaraz <menjaraz@gmail.com>
2023-10-19 22:58:48 +0300

    Add Project Euler problem 5 solution

---
+++ pe-0005/src/main.rs
@@ -0,0 +1,21 @@
+// Smallest Multiple
+// https://projecteuler.net/problem=5
+
+// extern crate num;
+
+use std::time::Instant;
+use num::integer::lcm;
+
+
+fn main() {
+    let start = Instant::now();
+    //
+    
+    let answer = (1..=20).fold(1, |acc, x| lcm(acc, x));
+    
+    //
+    let duration = start.elapsed();
+
+    println!("\nProject Euler #5\nAnswer: {}", answer);
+    println!("Elapsed time: {:?} milliseconds.\n", duration.as_millis()); 
+}
