c2ca45ef03d201a715f693e5838e421f5df2b197
menjaraz <menjaraz@gmail.com>
2023-10-19 18:59:19 +0300

    Add Project Euler problem 1 solution

---
+++ pe-0001/src/main.rs
@@ -0,0 +1,13 @@
+use std::time::Instant;
+use gcd::Gcd;
+
+fn main() {
+    let start = Instant::now();
+    //
+    let answer: u64 = (1..=999).filter(|&a| a.gcd(3 * 5) > 1).sum();
+    //
+    let duration = start.elapsed();
+
+    println!("\nProject Euler #1\nAnswer: {}", answer);
+    println!("Elapsed time: {:?} milliseconds.\n", duration.as_millis());
+}
