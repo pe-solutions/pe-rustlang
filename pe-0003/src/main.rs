1d4f7aeb21db9fc9b8a44f7b8df6e902c9850861
Menjanahary <menjaraz@gmail.com>
2023-11-01 13:33:10 +0400

    Update main.rs
    New version: Concise code, faster coder.

--- pe-0003/src/main.rs
+++ pe-0003/src/main.rs
@@ -1,43 +1,22 @@
 // Largest Prime Factor
 // https://projecteuler.net/problem=3
 
-use std::time::Instant;
-
-fn largest_prime_factor(mut n: u64) -> u64 {
-    let mut largest_factor = 0;
-
-    while n % 2 == 0 {
-        largest_factor = 2;
-        n /= 2;
-    }
-
-    let mut i = 3;
-    
-    while i * i <= n {
-        while n % i == 0 {
-            largest_factor = i;
-            n /= i;
-        }
-        i += 2;
-    }
-
-    if n > 2 {
-        largest_factor = n;
-    }
-
-    largest_factor
-}
-
-
 fn main() {
-    let start = Instant::now();
-    //
+    let start = std::time::Instant::now();
     
-    let answer: u64 = largest_prime_factor(600_851_475_143);
+    let mut answer: u64 = 600_851_475_143;
 
-    //
+    let mut i: u64 = 2;
+    while i * i < answer {
+        while answer % i == 0 {
+            answer /= i;
+        }
+
+        i += 1;
+    }
+
     let duration = start.elapsed();
 
     println!("\nProject Euler #3\nAnswer: {}", answer);
-    println!("Elapsed time: {:?} milliseconds.\n", duration.as_millis()); 
+    println!("Elapsed time: {:?} milliseconds.\n", duration.as_millis());
 }
