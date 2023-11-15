fbc02b086389ffb227f9828a0cb0dfb3085ba594
Menjanahary <menjaraz@gmail.com>
2023-11-15 12:10:00 +0400

    Update main.rs
    Replace with more efficient Brute force `Diophantine equations` solver

--- pe-0009/src/main.rs
+++ pe-0009/src/main.rs
@@ -1,15 +1,13 @@
 // Special Pythagorean Triplet
 // https://projecteuler.net/problem=9
 
-fn find_special_pythagorean_triplet() -> Option<u32> {
-    for a in 1..=1000 {
-        for b in a + 1..=1000 {
+fn find_triplet() -> Option<(u32, u32, u32)> {
+    for a in 1..334 {
+        for b in a + 1..(1000 - a) / 2 {
             let c = 1000 - a - b;
-
+            //
             if a * a + b * b == c * c {
-                return Some(a * b * c); // a * b * c;
-            } else if a * a + b * b > c * c {
-                break;
+                return Some((a, b, c));
             }
         }
     }
@@ -19,21 +17,16 @@ fn find_special_pythagorean_triplet() -> Option<u32> {
 
 fn main() {
     let start = std::time::Instant::now();
-    //
-    
-    match find_special_pythagorean_triplet() {
-        Some(answer) => {
-            let duration = start.elapsed();
-            
-            println!("\nProject Euler #9\nAnswer: {}", answer);
-            println!("Elapsed time: {:?} milliseconds.\n", duration.as_millis()); 
+
+    match find_triplet() {
+        Some((a, b, c)) => {
+            println!("\nProject Euler #9\nAnswer: {}", a*b*c);
         }
         None => {
-            let duration = start.elapsed();
-
-            println!("No special Pythagorean triplet found.");
-            println!("Elapsed time: {:?} milliseconds.\n", duration.as_millis()); 
+            println!("No solution found.");
         }
     }
-    
+
+    let duration = start.elapsed();
+    println!("Elapsed time: {:?} milliseconds.\n", duration.as_millis()); 
 }
