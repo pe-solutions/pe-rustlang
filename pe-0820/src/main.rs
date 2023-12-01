a852b06f402d111fe763a95254f5b7e9d77ac68a
Menjanahary <menjaraz@gmail.com>
2023-12-01 08:11:55 +0400

    Update main.rs
    Code Refactoring

--- pe-0820/src/main.rs
+++ pe-0820/src/main.rs
@@ -11,22 +11,23 @@ fn modular_exp(n: u64, p: u64, modulo: u64) -> u64 {
     }
 
     let tmp = modular_exp(n, p / 2, modulo) % modulo;
-
     (tmp * tmp) % modulo
+}
+
+fn calculate_nth_digit_sum(n: u64) -> u64 {
+    (1..=n)
+        .map(|i| modular_exp(10, n - 1, i) * 10 / i)
+        .sum()
 }
 
 fn main() {
     let start = std::time::Instant::now();
-    
-    let n: u64 = 10_000_000;
-    let mut answer: u64 = 0;
 
-    for i in 1..=n {
-        answer += modular_exp(10, n - 1, i) * 10 / i;
-    }
+    let n: u64 = 10_000_000;
+    let answer = calculate_nth_digit_sum(n);
 
     let duration = start.elapsed();
 
     println!("\nProject Euler #820\nAnswer: {}", answer);
-    println!("Elapsed time: {} milliseconds.\n", duration.as_millis()); 
+    println!("Elapsed time: {} milliseconds.\n", duration.as_millis());
 }
