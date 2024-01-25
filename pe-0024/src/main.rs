9e0412a38730fbff4cb8bacdbccc087eb7cfa834
Menjanahary <menjaraz@gmail.com>
2024-01-25 21:30:04 +0300

    Refactor pe-0024

--- pe-0024/src/main.rs
+++ pe-0024/src/main.rs
@@ -1,35 +1,24 @@
-use itertools::join;
+// Lexicographic Permutations
+// https://projecteuler.net/problem=24
 
-fn nth_permutation<T: Copy + Ord>(mut elements: Vec<T>, mut n: usize) -> Vec<T> {
-    let mut result = Vec::new();
-    let mut factorial = 1;
+use itertools::Itertools;
 
-    for i in 1..elements.len() {
-        factorial *= i;
-    }
+fn find_millionth_permutation() -> Result<String, &'static str> {
+    let permutation = (0..10)
+        .permutations(10)
+        .nth(1_000_000 - 1) // 1-based indexing, so subtract 1
+        .ok_or("Error: Could not find the millionth lexicographic permutation.")?;
 
-    while !elements.is_empty() {
-        let index = n / factorial;
-        result.push(elements.remove(index));
-        n %= factorial;
-
-        if !elements.is_empty() {
-            factorial /= elements.len();
-        }
-    }
-
-    result
+    let permutation_str = permutation.into_iter().map(|x| x.to_string()).collect::<String>();
+    
+    Ok(permutation_str)
 }
 
 fn main() {
     let start_time = std::time::Instant::now();
-
-    let src = (0..=9).collect::<Vec<_>>();
-
-    let permutation = nth_permutation(src, 1_000_000 - 1);
-
-    let answer = join(permutation, "");
-
+    
+    let answer = find_millionth_permutation().unwrap_or_default();
+    
     let duration = start_time.elapsed();
     
     println!("\nProject Euler #24\nAnswer: {}", answer);
