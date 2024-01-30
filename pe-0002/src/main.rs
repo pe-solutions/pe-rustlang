b26e2ee3c88bcf6959abb0781b7762edd2b65187
menjaraz <menjaraz@gmail.com>
2024-01-30 21:08:52 +0300

    Revert "Update main.rs"
    This reverts commit 5c9f705a76583eb285e977eec8eccf0baaac302d.

--- pe-0002/src/main.rs
+++ pe-0002/src/main.rs
@@ -1,32 +1,29 @@
 // Even Fibonacci Numbers
 // https://projecteuler.net/problem=2
 
-import std.stdio: writefln;
-import std.datetime.stopwatch: StopWatch;
-import std.range: recurrence;
-import std.algorithm.searching : until;
-import std.algorithm: sum;
+use std::time::Instant;
 
-ulong calculateSumOfEvenFibo(ulong limit)
-{
-    auto evenNumberedTermFib = recurrence!("4*a[n-1] + a[n-2]")(2uL, 8uL);
-    
-    return evenNumberedTermFib
-        .until!(x => x >= limit)
-        .sum;
-}
+fn main() {
+    let start = Instant::now();
+    //
+    let mut answer: u64 = 0;
 
-void main()
-{
-    StopWatch timer;
-    timer.start();
-    
-    const ulong UPPER_LIMIT = 4_000_000uL;
-    
-    auto answer = calculateSumOfEvenFibo(UPPER_LIMIT);
-    
-    timer.stop();
-    
-    writefln("\nProject Euler #2\nAnswer: %s", answer);
-    writefln("Elapsed time: %s milliseconds.\n", timer.peek.total!"msecs"());
+    let mut a: u64 = 1;
+    let mut b: u64 = 1;
+
+    while b < 4_000_000 {
+        if b % 2 == 0 {
+            answer += b;
+        }
+
+        let next = a + b;
+        
+        a = b;
+        b = next;
+    }
+    //
+    let duration = start.elapsed();
+
+    println!("\nProject Euler #2\nAnswer: {}", answer);
+    println!("Elapsed time: {:?} milliseconds.\n", duration.as_millis());
 }
