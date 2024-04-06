b08410a0555c97bdcfc45e93475a351b4aba2267
Menjanahary <menjaraz@gmail.com>
2024-04-06 14:48:34 +0400

    Refactor main.rs
    Using more concise Closed form

--- pe-0028/src/main.rs
+++ pe-0028/src/main.rs
@@ -1,16 +1,15 @@
 // Number spiral diagonals
 // https://projecteuler.net/problem=28
 
+// Closed form
+fn sum_of_spiral_diagonals(n: i64) -> i64 {
+    (2 * n.pow(3) + n.pow(2) + 4 * n - 3) / 3
+}
 
 fn main() {
     let start = std::time::Instant::now();
     
-    let mut answer = 1;
-
-    answer += (3..=1_001)
-        .step_by(2)
-        .map(|a:i32| 4 * a.pow(2) - 6 * a + 6)
-        .sum::<i32>();
+    let answer = sum_of_spiral_diagonals(1_001);
     
     let duration = start.elapsed();
 
