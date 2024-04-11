405f365ce8c1f5e89d2c03356a3c9c24bb7ab72b
Menjanahary <menjaraz@gmail.com>
2024-04-11 15:32:46 +0400

    Refactor main.rs
    Using more meaningful variable names

--- pe-0009/src/main.rs
+++ pe-0009/src/main.rs
@@ -1,18 +1,16 @@
 // Special Pythagorean Triplet
 // https://projecteuler.net/problem=9
 
-const P: u32 = 1_000u32;
+const PERIMETER: i32 = 1000;
 
-fn find_sp_triplet() -> Option<(u32, u32, u32)> {
-    for a in 1..P / 3 {
-        let n = P.pow(2) - 2 * P * a;
-        let d = 2 * P - 2 * a;
-        
-        if d != 0 && n % d == 0 {
-            let b = n / d;
-            let c = P - a - b;
-            
-            return Some((a, b, c));
+fn find_pythagorean_triplet() -> Option<(i32, i32, i32)> {
+    for a in 1..(PERIMETER / 3) {
+        for b in (a + 1)..(PERIMETER / 2) {
+            let c = PERIMETER - a - b;
+            //
+            if a.pow(2) + b.pow(2) == c.pow(2) {
+                return Some((a, b, c));
+            }
         }
     }
     
@@ -21,16 +19,13 @@ fn find_sp_triplet() -> Option<(u32, u32, u32)> {
 
 fn main() {
     let start = std::time::Instant::now();
-
-    match find_sp_triplet() {
-        Some((a, b, c)) => {
-            println!("\nProject Euler #9\nAnswer: {}", a*b*c);
-        }
-        None => {
-            println!("No solution found.");
-        }
+    
+    match find_pythagorean_triplet() {
+        Some((a, b, c)) => println!("\nProject Euler #9\nAnswer: {}", a * b * c),
+        None => println!("No triplet found!"),
     }
-
+    
     let duration = start.elapsed();
+    
     println!("Elapsed time: {:?} milliseconds.\n", duration.as_millis()); 
 }
