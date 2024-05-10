b260061804654dc24284ca4c217350cf52297be1
Menjanahary <menjaraz@gmail.com>
2024-05-10 20:22:10 +0400

    Update main.rs
    Refactor using Dynamic Programming

--- pe-0031/src/main.rs
+++ pe-0031/src/main.rs
@@ -1,28 +1,22 @@
 // Coin Sums
 // https://projecteuler.net/problem=31
 
-fn count_ways(amount: i32) -> i32 {
-    
-    const COINS: [i32; 8] = [1, 2, 5, 10, 20, 50, 100, 200];
-    
-    let mut ways: Vec<i32> = vec![0; (amount + 1) as usize];
-
-    ways[0] = 1;
-
-    for coin in COINS.iter() {
-        for i in *coin..=amount {
-            ways[i as usize] += ways[(i - coin) as usize];
-        }
-    }
-
-    ways[amount as usize]
-}
+const COINS: [usize; 8] = [1, 2, 5, 10, 20, 50, 100, 200];
+const TARGET: usize = 200;
 
 fn main() {
     let start = std::time::Instant::now();
-
-    let answer = count_ways(200);
-
+    
+    let mut ways = [0; TARGET + 1];
+    
+    ways[0] = 1;
+    
+    COINS.iter().for_each(|&coin| {
+        (coin..=TARGET).for_each(|amount| ways[amount] += ways[amount - coin])
+    });
+    
+    let answer = ways[TARGET];
+    
     println!("\nProject Euler #31\nAnswer: {}", answer);
 
     let duration = start.elapsed();
