439d8181b0207a59bab9e811cd6722c04f8e1c72
Menjanahary <menjaraz@gmail.com>
2024-12-24 12:08:26 +0400

    Minor Refactoring
    Getting rid of external crate(s)

--- pe-0005/src/main.rs
+++ pe-0005/src/main.rs
@@ -1,19 +1,27 @@
 // Smallest Multiple
-// https://projecteuler.net/problem=5
+// https://projecteuler.net/problem/5
 
 use std::time::Instant;
-use num::integer::lcm;
 
+fn gcd(a: u64, b: u64) -> u64 {
+    if b == 0 {
+        a
+    } else {
+        gcd(b, a % b)
+    }
+}
+
+fn lcm(a: u64, b: u64) -> u64 {
+    a * b / gcd(a, b)
+}
 
 fn main() {
     let start = Instant::now();
-    //
     
     let answer = (1..=20).fold(1, |acc, x| lcm(acc, x));
     
-    //
     let duration = start.elapsed();
 
     println!("\nProject Euler #5\nAnswer: {}", answer);
-    println!("Elapsed time: {:?} milliseconds.\n", duration.as_millis()); 
+    println!("Elapsed time: {:?} milliseconds.\n", duration.as_millis());
 }
