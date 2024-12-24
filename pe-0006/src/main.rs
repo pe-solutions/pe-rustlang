b795da7f063dd606ffd22fdfaa848a95c898b4dd
Menjanahary <menjaraz@gmail.com>
2024-12-24 12:14:24 +0400

    Minor Refactoring
    Extract function

--- pe-0006/src/main.rs
+++ pe-0006/src/main.rs
@@ -1,18 +1,21 @@
 // Sum Square Difference
-// https://projecteuler.net/problem=6
+// https://projecteuler.net/problem/6
 
 use std::time::Instant;
 
+fn sum_square_difference(n: i64) -> i64 {
+    let sum_of_squares: i64 = (1..=n).map(|x| x.pow(2)).sum();
+    let square_of_sums: i64 = (1..=n).sum::<i64>().pow(2);
+    
+    square_of_sums - sum_of_squares
+}
+
 fn main() {
     let start = Instant::now();
-    //
     
-    let sum_of_squares: i64 = (1..=100).sum::<i64>().pow(2);
-    let square_of_sums: i64 = (1..=100).map(|x: i64| x.pow(2)).sum();
+    let n: i64 = 100;
+    let answer = sum_square_difference(n);
     
-    let answer = sum_of_squares - square_of_sums;
-    
-    //
     let duration = start.elapsed();
 
     println!("\nProject Euler #6\nAnswer: {}", answer);
