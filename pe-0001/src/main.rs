2ad7f914034836f58ff38d6e515c002d659bd6b1
Menjanahary <menjaraz@gmail.com>
2024-12-24 08:58:18 +0400

    Minor Refactoring

--- pe-0001/src/main.rs
+++ pe-0001/src/main.rs
@@ -1,18 +1,17 @@
 // Multiples of 3 or 5
 // https://projecteuler.net/problem=1
 
-use std::time::Instant;
 
-fn solve(multiples: &[i32], limit: i32) -> i32 {
+fn multiples(factors: &[i32], limit: i32) -> i32 {
     (1..limit)
-        .filter(|&n| multiples.iter().any(|&m| n % m == 0))
+        .filter(|&n| factors.iter().any(|&m| n % m == 0))
         .sum()
 }
 
 fn main() {
-    let start = Instant::now();
+    let start = std::time::Instant::now();
     //
-    let answer = solve(&[3, 5], 1000);
+    let answer = multiples(&[3, 5], 1000);
     //
     let duration = start.elapsed();
     
