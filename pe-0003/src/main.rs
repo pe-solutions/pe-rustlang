5456b441e58b65b16e602cd5307a93bce3a0e1e9
Menjanahary <menjaraz@gmail.com>
2024-12-24 12:02:05 +0400

    Minor Refactoring
    Extract function.

--- pe-0003/src/main.rs
+++ pe-0003/src/main.rs
@@ -1,19 +1,22 @@
 // Largest Prime Factor
-// https://projecteuler.net/problem=3
+// https://projecteuler.net/problem/3
+
+fn largest_prime_factor(mut n: u64) -> u64 {
+    let mut i: u64 = 2;
+    while i * i <= n {
+        while n % i == 0 {
+            n /= i;
+        }
+        i += 1;
+    }
+    n
+}
 
 fn main() {
     let start = std::time::Instant::now();
     
-    let mut answer: u64 = 600_851_475_143;
-
-    let mut i: u64 = 2;
-    while i * i < answer {
-        while answer % i == 0 {
-            answer /= i;
-        }
-
-        i += 1;
-    }
+    let number: u64 = 600_851_475_143;
+    let answer = largest_prime_factor(number);
 
     let duration = start.elapsed();
 
